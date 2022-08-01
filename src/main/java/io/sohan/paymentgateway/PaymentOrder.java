d4084eb84e044478fdab912522ccb73334dbcdb6
Sohan Barman <barmansohan9@mail.com>
2022-08-01 13:48:48 +0530

    initial

---
+++ src/main/java/io/sohan/paymentgateway/PaymentOrder.java
@@ -0,0 +1,14 @@
+package io.sohan.paymentgateway;
+
+public class PaymentOrder {
+    int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+}
